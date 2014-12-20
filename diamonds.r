4bc36fb06831446f1faec7fcb3941824196b990b
David Kahle <david.kahle@gmail.com>
2014-12-20 16:56:55 -0600

    ggmosiac

--- diamonds.r
+++ diamonds.r
@@ -253,33 +253,66 @@ ggsave("disc-disc-4.png", width = width, height = height)
 
 
 
-clarityFreqs <- table(diamonds$clarity) / nrow(diamonds)
-cumClarFreqs <- unname(c(0,cumsum(clarityFreqs)))
 
-props <- ddply(diamonds, "clarity", function(df){
-  y <- unname(cumsum(c(0, table(df$cut) / nrow(df))))  
-  n <- length(y)
-  data.frame(ymin = y[1:(n-1)], ymax = y[2:n])
-})
-props$cut <- rep(levels(diamonds$cut), 8)
-props$cut <- factor(props$cut,
-  levels = levels(diamonds$cut), ordered = TRUE
-)
-props$xmin <- rep(cumClarFreqs[1:8], each = 5)
-props$xmax <- rep(cumClarFreqs[2:9], each = 5)
 
-# make x breaks
-xBreaks <- cumClarFreqs[1:8] + diff(cumClarFreqs)/2
-names(xBreaks) <- levels(diamonds$clarity)
-ggplot(data = props) +
-  geom_rect(aes(
-    xmin = xmin, xmax = xmax,
-    ymin = ymin, ymax = ymax,
-    fill = cut
-  ), color = "white") +
-  scale_color_discrete(guide = "none") +
-  scale_fill_discrete("Cut") +
-  scale_x_continuous("Clarity", breaks = xBreaks, expand = c(0,0)) +
-  scale_y_continuous("Relative Frequency", expand = c(0,0))
+
+
+
+
+
+
+
+
+
+
+
+
+ggmosaic <- function(x, y, ..., data, 
+  xlab = deparse(substitute(x)), 
+  ylab = deparse(substitute(y))
+){
+  
+  xName <- deparse(substitute(x))
+  xv <- data[,xName]
+  xLevels <- levels(xv)
+  
+  yName <- deparse(substitute(y))
+  yv <- data[,yName]
+  yLevels <- levels(yv)
+  
+  xFreqs <- table(xv) / length(xv)
+  cumxFreqs <- unname(c(0,cumsum(xFreqs)))
+  p <- length(cumxFreqs)
+  
+  props <- ddply(data, xName, function(df){
+    y <- unname(
+      cumsum(c(0, table(df[,yName]) / nrow(df)))
+    )  
+    n <- length(y)
+    data.frame(ymin = y[1:(n-1)], ymax = y[2:n])
+  })
+  
+  props[,yName] <- rep(yLevels, length(xLevels))
+  props[,yName] <- factor(props[,yName], levels = yLevels, ordered = TRUE  )  
+  
+  props$xmin <- rep(cumxFreqs[1:(p-1)], each = length(yLevels))
+  props$xmax <- rep(cumxFreqs[2:p], each = length(yLevels))
+  
+  xBreaks <- cumxFreqs[1:(p-1)] + diff(cumxFreqs)/2
+  names(xBreaks) <- xLevels
+  
+  ggplot(data = props) +
+    geom_rect(aes(
+      xmin = xmin, xmax = xmax,
+      ymin = ymin, ymax = ymax,
+      fill = cut
+    ), color = "white") +
+    scale_color_discrete(guide = "none") +
+    scale_fill_discrete(deparse(substitute(y))) +
+    scale_x_continuous(deparse(substitute(x)), breaks = xBreaks, expand = c(0,0)) +
+    scale_y_continuous("Relative Frequency", expand = c(0,0))
+}
+
+ggmosaic(clarity, cut, data = diamonds)
 ggsave("disc-disc-5.png", width = width, height = height)
 
