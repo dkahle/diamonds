3d51bd40441ddbeb9e990bd02277ccd2f8310276
David Kahle <david.kahle@gmail.com>
2014-12-22 13:24:55 -0600

    ggmosaic function, paper updates

--- diamonds.r
+++ diamonds.r
@@ -231,11 +231,7 @@ qplot(clarity, data = diamonds, geom = "bar",
 ggsave("disc-disc-3.png", width = width, height = height)
 
 
-# make mosaic plots, not native to ggplot2
-library(vcd)
-tab <- table(diamonds[,c("clarity","cut")])
-mosaicplot(tab)
-
+# pre-mosaic plot
 props <- ddply(diamonds, "clarity", function(df){
   table(df$cut) / nrow(df)
 })
@@ -252,67 +248,8 @@ ggplot(data = mprops) +
 ggsave("disc-disc-4.png", width = width, height = height)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-ggmosaic <- function(x, y, ..., data, 
-  xlab = deparse(substitute(x)), 
-  ylab = deparse(substitute(y))
-){
-  
-  xName <- deparse(substitute(x))
-  xv <- data[,xName]
-  xLevels <- levels(xv)
-  
-  yName <- deparse(substitute(y))
-  yv <- data[,yName]
-  yLevels <- levels(yv)
-  
-  xFreqs <- table(xv) / length(xv)
-  cumxFreqs <- unname(c(0,cumsum(xFreqs)))
-  p <- length(cumxFreqs)
-  
-  props <- ddply(data, xName, function(df){
-    y <- unname(
-      cumsum(c(0, table(df[,yName]) / nrow(df)))
-    )  
-    n <- length(y)
-    data.frame(ymin = y[1:(n-1)], ymax = y[2:n])
-  })
-  
-  props[,yName] <- rep(yLevels, length(xLevels))
-  props[,yName] <- factor(props[,yName], levels = yLevels, ordered = TRUE  )  
-  
-  props$xmin <- rep(cumxFreqs[1:(p-1)], each = length(yLevels))
-  props$xmax <- rep(cumxFreqs[2:p], each = length(yLevels))
-  
-  xBreaks <- cumxFreqs[1:(p-1)] + diff(cumxFreqs)/2
-  names(xBreaks) <- xLevels
-  
-  ggplot(data = props) +
-    geom_rect(aes(
-      xmin = xmin, xmax = xmax,
-      ymin = ymin, ymax = ymax,
-      fill = cut
-    ), color = "white") +
-    scale_color_discrete(guide = "none") +
-    scale_fill_discrete(deparse(substitute(y))) +
-    scale_x_continuous(deparse(substitute(x)), breaks = xBreaks, expand = c(0,0)) +
-    scale_y_continuous("Relative Frequency", expand = c(0,0))
-}
-
+# mosaic plot
+source("ggmosaic.r")
 ggmosaic(clarity, cut, data = diamonds)
 ggsave("disc-disc-5.png", width = width, height = height)
 
