429bffed0cbe3e657d3d627bc621f924d17d5c62
David Kahle <david.kahle@gmail.com>
2014-12-31 16:16:30 -0500

    add Paper/

--- diamonds.r
+++ diamonds.r
@@ -29,7 +29,7 @@ sum(round(table(diamonds$cut) / n, 3)) # take the .001 off of ideal
 
 qplot(cut, data = diamonds) +
    xlab("Cut") + ylab("Frequency")
-ggsave("barchart.pdf", width = width, height = height)
+ggsave("Paper/barchart.pdf", width = width, height = height)
 
 
 ggplot(
@@ -40,7 +40,7 @@ ggplot(
   coord_polar(theta = "y") +
   scale_x_continuous("", breaks = NULL) +
   scale_y_continuous("", breaks = NULL)
-ggsave("piechart.pdf", width = width, height = height)
+ggsave("Paper/piechart.pdf", width = width, height = height)
 
 #####################################################################
 #################### one-dim continuous          ####################
@@ -54,49 +54,49 @@ qplot(carat, 0L, data = fiftyDiamonds, size = I(8)) +
   geom_hline(yintercept = 0) +
   scale_x_continuous("Carat", lim = c(0,2.5)) +
   scale_y_continuous("", lim = c(-1,1), breaks = NULL)
-ggsave("1dscatterplot.pdf", width = width, height = height)
+ggsave("Paper/1dscatterplot.pdf", width = width, height = height)
 
 fiftyDiamonds$yValues <- runif(50, -1, 1)
 qplot(carat, yValues, data = fiftyDiamonds, size = I(8)) +
   geom_hline(yintercept = 0) +
   scale_x_continuous("Carat", lim = c(0,2.5)) +
   scale_y_continuous("", lim = c(-1,1), breaks = NULL)
-ggsave("jittering.pdf", width = width, height = height)
+ggsave("Paper/jittering.pdf", width = width, height = height)
 
 qplot(carat, yValues, data = fiftyDiamonds, size = I(8), alpha = I(.33)) +
   geom_hline(yintercept = 0) +
   scale_x_continuous("Carat", lim = c(0,2.5)) +
   scale_y_continuous("", lim = c(-1,1), breaks = NULL)
-ggsave("alpha.pdf", width = width, height = height)
+ggsave("Paper/alpha.pdf", width = width, height = height)
 
 qplot(carat, yValues, data = fiftyDiamonds, size = I(5), alpha = I(.33)) +
   geom_hline(yintercept = 0) +
   scale_x_continuous("Carat", lim = c(0,2.5)) +
   scale_y_continuous("", lim = c(-1,1), breaks = NULL)
-ggsave("resized.pdf", width = width, height = height)
+ggsave("Paper/resized.pdf", width = width, height = height)
 
 qplot(carat, runif(n, -1, 1), data = diamonds, size = I(5), alpha = I(.01)) +
   geom_hline(yintercept = 0) +
   scale_x_continuous("Carat", lim = c(0,2.5)) +
   scale_y_continuous("", lim = c(-1,1), breaks = NULL)
-ggsave("allscatter.png", width = width, height = height)
+ggsave("Paper/allscatter.png", width = width, height = height)
 
 
 ## histograms
 qplot(carat, data = diamonds) +
   scale_x_continuous("Carat", lim = c(0,5)) +
   ylab("Frequency")
-ggsave("histogram1.pdf", width = width, height = height)
+ggsave("Paper/histogram1.pdf", width = width, height = height)
 
 qplot(carat, data = diamonds, binwidth = 2) +
   scale_x_continuous("Carat", lim = c(0,5)) +
   ylab("Frequency")
-ggsave("histogram2.pdf", width = width, height = height)
+ggsave("Paper/histogram2.pdf", width = width, height = height)
 
 qplot(carat, data = diamonds, binwidth = .01) +
   scale_x_continuous("Carat", lim = c(0,5)) +
   ylab("Frequency") + geom_vline(xintercept = .9)
-ggsave("histogram3.pdf", width = width, height = height)
+ggsave("Paper/histogram3.pdf", width = width, height = height)
 
 slightlySmallerDiamonds <- subset(diamonds, .99 <= carat & carat < 1)
 nrow(slightlySmallerDiamonds)
@@ -144,12 +144,12 @@ ggplot() +
   ) +
   scale_x_continuous("Carat", lim = c(0,2.5)) +
   ylab("Density") 
-ggsave("kde.pdf", width = width, height = height)
+ggsave("Paper/kde.pdf", width = width, height = height)
 
 qplot(carat, ..density.., data = fiftyDiamonds, geom = "histogram") +
   scale_x_continuous("Carat", lim = c(0,2.5)) +
   ylab("Density")
-ggsave("kdeHist.pdf", width = width, height = height)
+ggsave("Paper/kdeHist.pdf", width = width, height = height)
 
 
 
@@ -160,18 +160,18 @@ ggsave("kdeHist.pdf", width = width, height = height)
 qplot(carat, price, data = diamonds) +
   scale_x_continuous("Carat") +
   scale_y_continuous("Price")
-ggsave("cont-cont-1.png", width = width, height = height)
+ggsave("Paper/cont-cont-1.png", width = width, height = height)
 
 qplot(carat, price, data = diamonds, alpha = I(.05), size = I(1)) +
   scale_x_continuous("Carat") +
   scale_y_continuous("Price")
-ggsave("cont-cont-2.png", width = width, height = height)
+ggsave("Paper/cont-cont-2.png", width = width, height = height)
 
 qplot(carat, price, data = diamonds, geom = "bin2d") +
   scale_x_continuous("Carat") +
   scale_y_continuous("Price") +
   scale_fill_continuous("Frequency")
-ggsave("cont-cont-3.png", width = width, height = height)
+ggsave("Paper/cont-cont-3.png", width = width, height = height)
 
 qplot(carat, price, data = diamonds, geom = "hex", bins = 100) +
   scale_x_continuous("Carat") +
@@ -181,7 +181,7 @@ qplot(carat, price, data = diamonds, geom = "hex", bins = 100) +
     values = c(0.00, 0.025, 0.15, 1.00),
     breaks = c(100,250,500,1000,2000)
   )
-ggsave("cont-cont-4.png", width = width, height = height)
+ggsave("Paper/cont-cont-4.png", width = width, height = height)
 
 
 qplot(carat, price, data = diamonds, geom = "hex", bins = 100) +
@@ -194,13 +194,13 @@ qplot(carat, price, data = diamonds, geom = "hex", bins = 100) +
     breaks = c(100,250,500,1000,2000)
   )
 
-ggsave("cont-cont-5.png", width = width, height = height)
+ggsave("Paper/cont-cont-5.png", width = width, height = height)
 
 ggplot(aes(x = carat, y = price), data = diamonds) +
   stat_density2d() +
   scale_x_continuous("Carat", lim = c(0,4)) +
   scale_y_continuous("Price", lim = c(0,18000))
-ggsave("cont-cont-6.png", width = width, height = height)
+ggsave("Paper/cont-cont-6.png", width = width, height = height)
 
 #####################################################################
 #################### two-dim disc-cont           ####################
@@ -209,22 +209,22 @@ ggsave("cont-cont-6.png", width = width, height = height)
 qplot(clarity, price, data = diamonds) +
   scale_x_discrete("Clarity") +
   scale_y_continuous("Price")
-ggsave("disc-cont-1.png", width = width, height = height)
+ggsave("Paper/disc-cont-1.png", width = width, height = height)
 
 qplot(clarity, price, data = diamonds, geom = "jitter", alpha = I(.05)) +
   scale_x_discrete("Clarity") +
   scale_y_continuous("Price")
-ggsave("disc-cont-2.png", width = width, height = height)
+ggsave("Paper/disc-cont-2.png", width = width, height = height)
 
 qplot(clarity, price, data = diamonds, geom = "boxplot") +
   scale_x_discrete("Clarity") +
   scale_y_continuous("Price")
-ggsave("disc-cont-3.pdf", width = width, height = height)
+ggsave("Paper/disc-cont-3.pdf", width = width, height = height)
 
 qplot(clarity, price, data = diamonds, geom = "violin") +
   scale_x_discrete("Clarity") +
   scale_y_continuous("Price")
-ggsave("disc-cont-4.pdf", width = width, height = height)
+ggsave("Paper/disc-cont-4.pdf", width = width, height = height)
 
 #####################################################################
 #################### complex                     ####################
@@ -238,7 +238,7 @@ qplot(clarity, price,
   scale_x_discrete("Clarity") +
   scale_y_continuous("Price") +
   scale_fill_discrete("Cut")
-ggsave("disc-cont-5.pdf", width = width, height = height)
+ggsave("Paper/disc-cont-5.pdf", width = width, height = height)
 
 #####################################################################
 #################### two-dim disc-disc           ####################
@@ -249,15 +249,15 @@ qplot(clarity, cut, data = diamonds, geom = "jitter",
   ) +
   scale_x_discrete("Clarity") +
   scale_y_discrete("Cut")
-ggsave("disc-disc-1.png", width = width, height = height)
+ggsave("Paper/disc-disc-1.png", width = width, height = height)
 
 qplot(clarity, data = diamonds, geom = "bar", 
   fill = cut, position = "dodge")
-ggsave("disc-disc-2.png", width = width, height = height)
+ggsave("Paper/disc-disc-2.png", width = width, height = height)
 
 qplot(clarity, data = diamonds, geom = "bar", 
   fill = cut, position = "stack")
-ggsave("disc-disc-3.png", width = width, height = height)
+ggsave("Paper/disc-disc-3.png", width = width, height = height)
 
 
 # pre-mosaic plot
@@ -274,11 +274,11 @@ ggplot(data = mprops) +
   scale_x_discrete("Clarity") +
   scale_y_continuous("Relative Frequency") +
   scale_fill_discrete("Cut")
-ggsave("disc-disc-4.png", width = width, height = height)
+ggsave("Paper/disc-disc-4.png", width = width, height = height)
 
 
 # mosaic plot
 source("ggmosaic.r")
 ggmosaic(clarity, cut, data = diamonds)
-ggsave("disc-disc-5.png", width = width, height = height)
+ggsave("Paper/disc-disc-5.png", width = width, height = height)
 
